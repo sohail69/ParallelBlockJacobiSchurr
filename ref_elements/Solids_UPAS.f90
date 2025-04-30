2ea4b63742c8b1cdc89cb4d6bb0b16df08597b07
sar-local <sar-local@localhost.localdomain>
2025-04-30 03:32:54 +0100

    modified:   ref_elements/Solids_UPAS.f90 	new file:   ref_elements/tmp_solid.f90

--- ref_elements/Solids_UPAS.f90
+++ ref_elements/Solids_UPAS.f90
@@ -13,7 +13,7 @@ SUBROUTINE STATIC_SOLIDUPAS_ELEMENT(Km, Rm, utemp, astrain, fibre, MATPROP   &
                                   , nprop, ntots, ndofU, ndofP, ndim , nst   &
                                   , nip, nodU, nodp)
   IMPLICIT NONE
-  INTEGER                :: Ig, i, j, k, l, p, q, m, n, o, s, t;
+  INTEGER                :: Iel, Ig, i, j, k, l, p, q, m, n, o, s, t;
   INTEGER,  INTENT(IN)   :: nprop, ntots, ndim, nst;
   INTEGER,  INTENT(IN)   :: nip, nodU, nodp, ndofU, ndofP
   REAL(iwp),INTENT(IN)   :: astrain(nodU,nel_pp), fibre(ndim,ndim,nel_pp);
@@ -22,16 +22,18 @@ SUBROUTINE STATIC_SOLIDUPAS_ELEMENT(Km, Rm, utemp, astrain, fibre, MATPROP   &
   REAL(iwp),INTENT(IN)   :: dNi_p(ndim,nodP,nip), Ni_p(nodP,nip)
   REAL(iwp),INTENT(IN)   :: dNi_u(ndim,nodU,nip), Ni_u(nodU,nip)
   REAL(iwp),INTENT(INOUT):: Km(ntots,ntots,nel_pp), Rm(ntots,nel_pp);
-  REAL(iwp),PARAMETER    :: zero = 0._iwp, one = 1._iwp;
+  REAL(iwp),PARAMETER    :: zero = 0._iwp, one = 1._iwp, two = 2._iwp, three=3._iwp;
 
   !Deformation related terms
-  REAL(iwp):: S_kk(nip), C_kkpp(nip)
+  REAL(iwp):: PK2, Ctang, S_bulk, K_bulk
+  REAL(iwp):: S_kk(nip), C_kkpp(nip), auxm(ndim,nodU), pxm(nodP)
   REAL(iwp):: C_ijkl(nst,nst,nip), S_ij(nst,nip), CdefInv(ndim,ndim,nip)
   REAL(iwp):: strain, gamma_f, gamma_n, gamma_s, press(nip);
   REAL(iwp):: Fdef(ndim,ndim), Fedef(ndim,ndim), F0def(ndim,ndim);
   REAL(iwp):: Cdef(ndim,ndim), F0inv(ndim,ndim);
   REAL(iwp):: dE(nst,ndofU,nip), d2E(nst,ndofU,ndofU,nip);
 
+  INTEGER  :: Map(ndim+1,nodU);
 
   !Shape function and derivatives
   REAL(iwp):: Jac(ndim,ndim), Jacinv(ndim,ndim), det(nip);
@@ -49,203 +51,184 @@ SUBROUTINE STATIC_SOLIDUPAS_ELEMENT(Km, Rm, utemp, astrain, fibre, MATPROP   &
   Jac    = zero;  Jacinv  = zero;   det    = zero;
   Cdef   = zero;  CdefInv = zero;   Fdef   = zero;
 
-!===
-!
-! Selective reduced integration
-!
-!====
-ELEMENTS:DO Iel = 1, nel_pp
-  !-----
-  ! Calculate the stress,
-  ! tangent-stiffness and
-  ! deformation measures at
-  ! the gauss-points
-  !-----
-  CALL SDF_MAP(Map, nod, ndim)
-  DO I=1,ndim
-    auxm(I,:) = utemp(Map(I,:));
-  ENDDO
-
-  GAUSS_PTS1: DO Ig = 1,nip
-    ! Calculate the coordinate
-    ! tranform from ref-to-physical
-    Jac = MATMUL(dNi(:,:,Ig), coord(:,:))
-    CALL Invert2(Jac, Jacinv, nDIM)
-    det(Ig) = DETERMINANT(Jac)
-
-    ! Calculate the active strain
-    ! components of the tensors
-    strain = DOT_PRODUCT(Ni_u(:,Ig),gama(:))
-    gamma_f =  -strain;
-    gamma_n =  4._iwp*gamma_f;
-    gamma_s = (one/((one + gamma_f)*(one + gamma_n))) - one;
-    DO i = 1,ndim
-      DO j = 1,ndim
-        F0def(i,j) = Kdelta(i,j) + gamma_f*fibre(i,1)*fibre(j,1)
-        IF(ndim > 2) F0def(i,j) = F0def(i,j) + gamma_s*fibre(i,2)*fibre(j,2)
-        IF(ndim > 2) F0def(i,j) = F0def(i,j) + gamma_n*fibre(i,3)*fibre(j,3)
-      ENDDO
-    ENDDO GAUSS_PTS1
-    CALL INVERT2(F0def,F0inv,ndim)
-
-   ! Calculate the standard
-   ! displacement component
-    Fdef = zero;
-    DO I = 1,ndim; DO J = 1,ndim
-      Fdef(I,J) = Fdef(I,J) + DOT_PRODUCT(dNi_u(J,:,Ig),auxm(I,:));
-      IF(I==J) Fdef(I,J) = Fdef(I,J) + one;
-    ENDDO; ENDDO
-
-    ! Calculate the stress, stiffness
-    ! and strain measures/derivatives
-    Fedef = MATMUL(Fdef,F0Inv)
-    CALL GREENLAGRANGE_DERIVATIVES_AS(Fdef,F0Inv,SDF,dE(:,:,Ig),d2E(:,:,:,Ig),ndim,ndofU,nodU,nst)
-    CALL INVERT2(Cdef,CdefInv(:,:,Ig),ndim); 
-    CALL MATMOD_NOH(C_ijkl(:,:,Ig),S_ij(:,Ig),Matprops,Fdef,ndim,nst,nprop)
-    DO m = 1,ndim
-      S_kk(Ig) = S_kk(Ig) + S_ij(m,Ig);
-      DO n = 1,ndim
-        C_kkpp(Ig) = C_kkpp(Ig) + C_ijkl(m,n,Ig)
-      ENDDO
+  !===
+  !
+  ! Integrate all the elements
+  !
+  !====
+  ELEMENTS:DO Iel = 1, nel_pp
+    !-----
+    ! Calculate the stress,
+    ! tangent-stiffness and
+    ! deformation measures at
+    ! the gauss-points
+    !-----
+    CALL SDF_MAP(Map, nodU, ndim+1)
+    DO I=1,ndim
+      auxm(I,:) = utemp(Map(I,:),Iel);
     ENDDO
+    pxm = utemp(Map(ndim+1,1:nodP),Iel);
 
-    !Calculating the pressure
-    !at the Gauss points
-    press(Ig) = DOT_PRODUCT()
-  ENDDO
+    GAUSS_PTS1: DO Ig = 1,nip
+      ! Calculate the coordinate
+      ! tranform from ref-to-physical
+      Jac = MATMUL(dNi(:,:,Ig), coord(:,:,Iel))
+      CALL Invert2(Jac, Jacinv, nDIM)
+      det(Ig) = DETERMINANT(Jac)
 
-!xCoord(ndim,nod,nel_pp)
-!dNi_p(ndim,nodP,nip), Ni_p(nodP,nip)
-!dNi_u(ndim,nodU,nip), Ni_u(nodU,nip)
-!nip, nodU, nodp, ndofU, ndofP
+      ! Calculate the active strain
+      ! components of the tensors
+      strain = DOT_PRODUCT(Ni_p(:,Ig),  astrain(1:nodP,Iel) )
+      gamma_f =  -strain;
+      gamma_n =  4._iwp*gamma_f;
+      gamma_s = (one/((one + gamma_f)*(one + gamma_n))) - one;
+      DO i = 1,ndim
+        DO j = 1,ndim
+          F0def(i,j) = Kdelta(i,j) + gamma_f*fibre(i,1,Iel)*fibre(j,1,Iel)
+          IF(ndim > 1) F0def(i,j) = F0def(i,j) + gamma_s*fibre(i,2,Iel)*fibre(j,2,Iel)
+          IF(ndim > 2) F0def(i,j) = F0def(i,j) + gamma_n*fibre(i,3,Iel)*fibre(j,3,Iel)
+        ENDDO
+      ENDDO GAUSS_PTS1
+      CALL INVERT2(F0def,F0inv,ndim)
 
-  !-----
-  ! Integrate the residuals
-  ! and the Jacobians
-  !-----
-  DO M = 1,ntots
-    GAUSS_PTS2: DO Ig = 1,nip
+      ! Calculate the standard
+      ! displacement component
+      Fdef = zero;
+      DO I = 1,ndim; DO J = 1,ndim
+        Fdef(I,J) = Fdef(I,J) + DOT_PRODUCT(dNi_u(J,:,Ig),auxm(I,:));
+        IF(I==J) Fdef(I,J) = Fdef(I,J) + one;
+      ENDDO; ENDDO
 
-      ! UU-block
-      DO s = 1,nst
-        CALL VOIGHT_ITERATOR(s,I,J,nst)
-        Rm(M) = Rm(M) + dE(s,M,Ig)*S_ij(s,Ig)*det(Ig)*weights(Ig);
-                          CdefInv(I,J,Ig)
+      ! Calculate the stress, stiffness
+      ! and strain measures/derivatives
+      Fedef = MATMUL(Fdef,F0Inv)
+      CALL GREENLAGRANGE_DERIVATIVES_AS(Fdef,F0Inv,dNi,dE(:,:,Ig),d2E(:,:,:,Ig),Map(1:ndim,:),ndim,ndofU,nodU,nst)
+      CALL INVERT2(Cdef,CdefInv(:,:,Ig),ndim); 
+      CALL MATMOD_NOH(C_ijkl(:,:,Ig),S_ij(:,Ig),Matprops,Fdef,ndim,nst,nprop)
 
-
-        Km(M,:) = Km(M,:) + S_ij(s,Ig)*d2E(s,:,M,Ig)*det(Ig)*weights(Ig);
-        DO t = 1,nst
-          Km(M,:) = Km(M,:) + dE(s,M,Ig)*C_ijkl(s,t,Ig)*dE(t,:,Ig)*det(Ig)*weights(Ig);
+      ! Calculate the volumetric stress
+      ! and the Bulk stiffness at the
+      ! Gauss-Ptns
+      DO m = 1,ndim
+        S_kk(Ig) = S_kk(Ig) + S_ij(m,Ig);
+        DO n = 1,ndim
+          C_kkpp(Ig) = C_kkpp(Ig) + C_ijkl(m,n,Ig)
         ENDDO
       ENDDO
 
-
-    END DO GAUSS_PTS2
-  ENDDO
-
-
-ENDDO ELEMENTS
-
-
-
-    J11 = J11 + MATMUL(TRANSPOSE(dE),MATMUL(C_ijkl,dE))
-    DO s =1,nst
-      R1  = R1  + S_ij(s)*dE(s,:)
-      J11 = J11 + S_ij(s)*d2E(s,:,:)
+      !Calculating the pressure
+      !at the Gauss points
+      press(Ig) = DOT_PRODUCT(Ni_p(:,Ig),pxm)
     ENDDO
 
-    CALL SHAPE_FUN(funP,points2,Ig);
-    funnyP(1,:) = funP(abaqustosg)
-    Cdef = MATMUL(TRANSPOSE(Fedef),Fedef);
-    CALL INVERT2(Cdef,CdefInv,ndim);
-    J3 = determinant(Fedef)
-    pressure = DOT_PRODUCT(funP(abaqustosg),ptemp)
-    DO s =1,nst
-      CALL VOIGHT_ITERATOR(s,i,j,nst);
-      R1  = R1  + pressure*CdefInv(i,j)*dE(s,:)
-      J11 = J11 + pressure*CdefInv(i,j)*d2E(s,:,:)
-      DO m = 1,ndofU;
-        J12(m,:)=J12(m,:) + CdefInv(i,j)*funnyP(1,:)*dE(s,m)
-        DO n = 1,ndofU; DO t =1,nst
-          CALL VOIGHT_ITERATOR(t,k,l,nst);
-          J11(m,n)=J11(m,n)-2._iwp*pressure*CdefInv(i,k)*CdefInv(j,l) &
-                                  *dE(s,m)*dE(t,n)
-        ENDDO; ENDDO
-      ENDDO
-    ENDDO
+    !-----
+    ! Integrate the residuals
+    ! and the Jacobians
+    !-----
+    ! UU -Block
+    ! Pure displacement block
+    !
+    DO M = 1,ndofU
+      GAUSS_PTS2: DO Ig = 1,nip
+        DO s = 1,nst; CALL VOIGHT_ITERATOR(s,I,J,nst)
+          PK2 = S_ij(s,Ig) + press(Ig)*CdefInv(I,J,Ig) - S_kk(Ig)*Kdelta(I,J)/three;
+          Rm(M) = Rm(M) + dE(s,M,Ig)*PK2*det(Ig)*weights(Ig);
+          Km(M,1:ndofU) = Km(M,1:ndofU) + PK2*d2E(s,:,M,Ig)*det(Ig)*weights(Ig);
 
-  Y     = 1.00_iwp;
-  nu    = 0.49999_iwp;
-  mu    = (4.6_iwp/2.2_iwp)/(Y/(2._iwp+2._iwp*nu))
-  lmbda = Y*nu/((1+nu)*(1._iwp-2._iwp*nu))
-
-
-    R2  = R2  + (DLOG(J3) - (pressure/lmbda))*funnyP(1,:)
-    J22 = (-J3/lmbda)*MATMUL(TRANSPOSE(funnyP(:,:)),funnyP(:,:))
-
-
-  !-----
-  ! Integrate the residuals
-  ! and the Jacobians
-  !-----
-  DO M = 1,ntots
-    GAUSS_PTS2: DO Ig = 1,nip
-      DO I = 1,nst
-        Rm(M) = Rm(M) + dE(I,M,Ig)*S_ij(I,Ig)*det(Ig)*weights(Ig);
-        Km(M,:) = Km(M,:) + S_ij(I,Ig)*d2E(I,:,M,Ig)*det(Ig)*weights(Ig);
-        DO J = 1,nst
-          Km(M,:) = Km(M,:) + dE(I,M,Ig)*C_ijkl(I,J,Ig)*dE(J,:,Ig)*det(Ig)*weights(Ig);
+          DO t = 1,nst; CALL VOIGHT_ITERATOR(t,K,L,nst)
+            Ctang = C_ijkl(s,t,Ig) - two* press(Ig)*CdefInv(K,I,Ig)*CdefInv(J,L,Ig)
+            Ctang = Ctang - C_kkpp(Ig)*Kdelta(I,J)*Kdelta(K,L)/three;
+            Km(M,1:ndofU) = Km(M,1:ndofU) + dE(s,M,Ig)*Ctang*dE(t,:,Ig)*det(Ig)*weights(Ig);
+          ENDDO
         ENDDO
-      ENDDO
-    END DO GAUSS_PTS2
-  ENDDO
+      END DO GAUSS_PTS2
+    ENDDO
+
+    ! UP-PU-Block(s)
+    ! Pressure-displacement mixed block
+    !
+    DO M = (ndofU+1),(ndofU+ndofP)
+      GAUSS_PTS2: DO Ig = 1,nip
+        DO s = 1,nst; CALL VOIGHT_ITERATOR(s,I,J,nst)
+          DO t = 1,nst; CALL VOIGHT_ITERATOR(t,K,L,nst)
+            Ctang = C_ijkl(s,t,Ig) - two* press(Ig)*CdefInv(K,I,Ig)*CdefInv(J,L,Ig)
+            Ctang = Ctang - C_kkpp(Ig)*Kdelta(I,J)*Kdelta(K,L)/three;
+
+S_bulk = S_kk(Ig)*
+K_bulk
 
 
-  !
-  ! Residual and Jacobian eliminated Pressure DOFs
-  !
-  DO i = (ndofU+ndofP+1),ntots
-    Rm(I)   = zero;
-    Km(I,:) = zero;
-    Km(I,I) = one;
-  ENDDO
+          PK2 = S_ij(s,Ig) + press(Ig)*CdefInv(I,J,Ig) - S_kk(Ig)*Kdelta(I,J)/three;
+          Rm(M) = Rm(M) + dE(s,M,Ig)*PK2*det(Ig)*weights(Ig);
+          Km(M,:) = Km(M,:) + PK2*d2E(s,:,M,Ig)*det(Ig)*weights(Ig);
 
+
+            Km(M,:) = Km(M,:) + dE(s,M,Ig)*Ctang*dE(t,:,Ig)*det(Ig)*weights(Ig);
+          ENDDO
+        ENDDO
+      END DO GAUSS_PTS2
+    ENDDO
+
+
+!  INTEGER,  INTENT(IN)   :: nprop, ntots, ndim, nst;
+!  INTEGER,  INTENT(IN)   :: nip, nodU, nodp, ndofU, ndofP
+
+    ! PP-Block
+    ! Pure pressure block
+    !
+    DO M = 1,ndofU
+      GAUSS_PTS2: DO Ig = 1,nip
+        DO s = 1,nst; CALL VOIGHT_ITERATOR(s,I,J,nst)
+          PK2 = S_ij(s,Ig) + press(Ig)*CdefInv(I,J,Ig) - S_kk(Ig)*Kdelta(I,J)/three;
+          Rm(M) = Rm(M) + dE(s,M,Ig)*PK2*det(Ig)*weights(Ig);
+          Km(M,:) = Km(M,:) + PK2*d2E(s,:,M,Ig)*det(Ig)*weights(Ig);
+
+          DO t = 1,nst; CALL VOIGHT_ITERATOR(t,K,L,nst)
+            Ctang = C_ijkl(s,t,Ig) - two* press(Ig)*CdefInv(K,I,Ig)*CdefInv(J,L,Ig)
+            Ctang = Ctang - C_kkpp(Ig)*Kdelta(I,J)*Kdelta(K,L)/three;
+            Km(M,:) = Km(M,:) + dE(s,M,Ig)*Ctang*dE(t,:,Ig)*det(Ig)*weights(Ig);
+          ENDDO
+        ENDDO
+      END DO GAUSS_PTS2
+    ENDDO
+
+
+  ENDDO ELEMENTS
   RETURN
 END SUBROUTINE STATIC_SOLIDUPAS_ELEMENT
 
 !-----------------
 ! Green-Lagrange-strain derivatives Active strain Variant
 !-----------------
-SUBROUTINE GREENLAGRANGE_DERIVATIVES_AS(Fdef,F0Inv,SDF,dE,d2E,ndim,ntots,nod,nst)
+SUBROUTINE GREENLAGRANGE_DERIVATIVES_AS(Fdef,F0Inv,dNi,dE,d2E,Map,ndim,ntots,nod,nst)
   IMPLICIT NONE
-   INTEGER                :: i, j, k, l, p, q, m, n;
-  INTEGER,   INTENT(IN)   :: ndim, ntots, nod, nst;
-  REAL(iwp), INTENT(IN)   :: Fdef(ndim,ndim), F0Inv(ndim,ndim),SDF(ndim,ndim,ntots);
+  INTEGER                :: i, j, k, l, p, q, r, m, n;
+  INTEGER,   INTENT(IN)   :: nod, ntots, ndim, nst;
+  INTEGER,   INTENT(IN)   :: Map(ndim,nod);
+  REAL(iwp), INTENT(IN)   :: Fdef(ndim,ndim), F0Inv(ndim,ndim), dNi(ndim,nod);
   REAL(iwp), INTENT(INOUT):: d2E(nst,ntots,ntots), dE(nst,ntots);
   REAL(iwp)               :: a, d2temp(ntots,ntots), d1temp(ntots);
 
-  d2E = 0._iwp; d2temp = 0._iwp;
-  dE = 0._iwp;  d1temp = 0._iwp;
-  DO m = 1,nst
-    CALL VOIGHT_ITERATOR(m, i, j, nst)
-    DO n = 1,nst
-      CALL VOIGHT_ITERATOR(n, p, q, nst)
-      DO k = 1,ndim
-        d1temp = d1temp + (SDF(k,p,:)*Fdef(k,q) + SDF(k,q,:)*Fdef(k,p))
-        DO l = 1,ntots
-          d2temp(l,:) = d2temp(l,:) + (SDF(k,p,l)*SDF(k,q,:)+SDF(k,q,l)*SDF(k,p,:))
-        ENDDO
-      ENDDO
-      a = 0.5_iwp*F0Inv(p,i)*F0Inv(q,j)
-      dE(m,:) = dE(m,:) + a*d1temp
-      IF(p/=q) dE(m,:) = dE(m,:) + a*d1temp
-      d2E(m,:,:) = d2E(m,:,:) + a*d2temp
-      IF(p/=q) d2E(m,:,:) = d2E(m,:,:) + a*d2temp
+  d2E = 0._iwp; dE = 0._iwp;
+  DO m = 1,nst; CALL VOIGHT_ITERATOR(m, i, j, nst)
+    DO n = 1,nst; CALL VOIGHT_ITERATOR(n, p, q, nst)
       d2temp = 0._iwp
       d1temp = 0._iwp
+      a = 0.5_iwp*F0Inv(p,i)*F0Inv(q,j)
+      IF(p/=q) a = F0Inv(p,i)*F0Inv(q,j)
+      DO k = 1,ndim
+        d1temp(Map(k,:)) = d1temp(Map(k,:)) + (dNi(p,:)*Fdef(k,q) + dNi(q,:)*Fdef(k,p))
+        DO l = 1,ndim
+          DO r = 1,nod
+            d2temp(Map(l,r),Map(k,:)) = d2temp(Map(l,r),Map(k,:)) + (dNi(p,r)*dNi(q,:) + dNi(q,r)*dNi(p,:))
+          ENDDO
+        ENDDO
+      ENDDO
+      dE(m,:) = dE(m,:) + a*d1temp
+      d2E(m,:,:) = d2E(m,:,:) + a*d2temp
     ENDDO
-    IF(i/=j) dE(m,:) = 2._iwp*dE(m,:);
-    IF(i/=j) d2E(m,:,:) = 2._iwp*d2E(m,:,:);
+    IF(I/=J) dE(m,:) = 2._iwp*dE(m,:);
+    IF(I/=J) d2E(m,:,:) = 2._iwp*d2E(m,:,:);
   ENDDO
   RETURN
 ENDSUBROUTINE GREENLAGRANGE_DERIVATIVES_AS
@@ -272,11 +255,9 @@ SUBROUTINE MATMOD_NOH(C_ijkl,S_ij,Matprops,Fdef,ndim,nst,nprop)
    lmbda = Y*nu/((1+nu)*(1._iwp-2._iwp*nu))
    Cdef = MATMUL(TRANSPOSE(Fdef),Fdef)
    CALL INVERT2(Cdef,C_inv,ndim)
-   DO m = 1,nst
-     CALL VOIGHT_ITERATOR(m, i, j, nst)
+   DO m = 1,nst; CALL VOIGHT_ITERATOR(m, i, j, nst)
      S_ij(m) = mu*(Kdelta(i,j) - C_inv(i,j));
-     DO n = 1,nst
-       CALL VOIGHT_ITERATOR(n, k, l, nst)
+     DO n = 1,nst; CALL VOIGHT_ITERATOR(n, k, l, nst)
        C_ijkl(m,n) =  2._iwp*mu*C_inv(i,k)*C_inv(j,l);
      ENDDO
    ENDDO
@@ -284,7 +265,7 @@ SUBROUTINE MATMOD_NOH(C_ijkl,S_ij,Matprops,Fdef,ndim,nst,nprop)
 ENDSUBROUTINE MATMOD_NOH
 
 !-------------------------------------
-!  SDF map
+! SDF map
 !-------------------------------------
 SUBROUTINE SDF_MAP(Map, nod, ndim)
   IMPLICIT NONE
